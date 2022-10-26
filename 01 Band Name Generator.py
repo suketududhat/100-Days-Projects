e0b1d4fce126fe683097ce25680171cf0e3c0c97
suketududhat <suk2dothat@gmail.com>
2022-10-25 20:09:26 -0700

    First day completed; learned variable assignment and input()

---
+++ 01 Band Name Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What city did you grow up in?")
+pet = input("What is your pet's name?")
+
+print(f"Your band name could be {city} {pet}")
