4a6ec2dc5442e77aae735d4762d1b3348cf1e076
suketududhat <suk2dothat@gmail.com>
2022-11-07 15:11:48 -0800

    Practice calling OOP Classes and Attributes

--- Intermediate/16 OOP/main.py
+++ Intermediate/16 OOP/main.py
@@ -6,11 +6,17 @@ money_machine = MoneyMachine()
 menu = Menu()
 coffee_maker = CoffeeMaker()
 
-user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
-if user_input == "report":
-    coffee_maker.report()
-elif user_input == "off":
-    None
-else:
-    drink = menu.find_drink(user_input)
-    print(MenuItem(drink))
+is_on = True
+
+while is_on:
+    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_input == "report":
+        coffee_maker.report()
+    elif user_input == "off":
+        is_on = False
+    else:
+        drink = menu.find_drink(user_input)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(
+            drink.cost
+        ):
+            coffee_maker.make_coffee(drink)
