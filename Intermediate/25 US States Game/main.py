58df76660042d0a07c5423caa0b361dd4837184c
suketududhat <suk2dothat@gmail.com>
2022-11-12 11:54:04 -0800

    Pandas practice

--- Intermediate/25 US States Game/main.py
+++ Intermediate/25 US States Game/main.py
@@ -3,8 +3,18 @@
 
 # print(data)
 
-import pandas
+import pandas as pd
 
-df = pandas.read_csv("Intermediate/25 US States Game/weather_data.csv")
-temps = df["temp"].to_list()
-print(df["temp"].mean())
+df = pd.read_csv(
+    "Intermediate/25 US States Game/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv"
+)
+
+# temps = df["temp"].to_list()
+# print(df["temp"].mean())
+
+fur_colors = df["Primary Fur Color"].unique().tolist()
+
+
+grey_squirrels = df.loc[df["Primary Fur Color"] == "Gray", ["Primary Fur Color"]]
+grey_squirrels = grey_squirrels.count()
+print(fur_colors)
