1f9d9728b0ce41fce22990236ec1420bd592be8e
suketududhat <suk2dothat@gmail.com>
2022-11-23 16:29:26 -0800

    Quiz app with Tkinter, API and OOP

---
+++ Intermediate+/34 Quiz App/main.py
@@ -0,0 +1,29 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+import requests
+from ui import QuizInterface
+
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
---
+++ Intermediate+/34 Quiz App/ui.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzie")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150, 125, text="test", fill=THEME_COLOR, font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, sticky="EW", pady=50)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        # self.score_label.config(padx=20, pady=20)
+        self.score_label.grid(row=0, column=1)
+
+        true_img = PhotoImage(file="Intermediate+/34 Quiz App/images/true.png")
+        self.true_button = Button(
+            image=true_img, highlightthickness=0, command=self.get_next_question
+        )
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="Intermediate+/34 Quiz App/images/false.png")
+        self.false_button = Button(
+            image=false_img, highlightthickness=0, command=self.get_next_question
+        )
+        self.false_button.grid(row=2, column=1)
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
